acdd845cad1e11f237f3ca411157aa78b70c16e7
Ashish D'sa <ashish.d@hypersonix.io>
2021-05-06 20:18:27 +0530

    Updated src/screens/Appointment.tsx

---
+++ src/screens/Appointment.tsx
@@ -0,0 +1,26 @@
+import { NavigationProp } from "@react-navigation/native";
+import React, { useRef } from "react";
+import { Appbar, withTheme } from "react-native-paper";
+import WebView from "react-native-webview";
+
+const Appointment = ({ navigation, theme }: { navigation: NavigationProp<any>; theme: ReactNativePaper.Theme }) => {
+  const webViewRef = useRef(null);
+
+  return (
+    <>
+      <Appbar.Header>
+        <Appbar.BackAction onPress={() => navigation.goBack()} />
+        <Appbar.Content title="Vaccination Appointment" subtitle="" />
+      </Appbar.Header>
+      <WebView
+        source={{ uri: "https://selfregistration.cowin.gov.in/" }}
+        incognito={true}
+        javaScriptEnabled={true}
+        //@ts-ignore
+        ref={ref => (webViewRef.current = ref)}
+      />
+    </>
+  );
+};
+
+export default withTheme(Appointment);
