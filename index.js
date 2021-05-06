689017428af0f7b6aa54ca35d7dc5ccbb3c058e3
Ashish D'sa <ashish.d@hypersonix.io>
2021-05-06 20:18:25 +0530

    Updated index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * @format
+ */
+
+import { AppRegistry } from "react-native";
+import App from "./App";
+import { name as appName } from "./app.json";
+import { createChannel, registerPushNotifications } from "./src/services/notifications";
+import ReactNativeForegroundService from "@supersami/rn-foreground-service";
+
+registerPushNotifications();
+createChannel();
+// Register the service
+ReactNativeForegroundService.register();
+AppRegistry.registerComponent(appName, () => App);
