2e19d4a345ba0cc1e1e2572363e49ac1481bb041
Ashish D'sa <ashish.d@hypersonix.io>
2021-05-06 20:18:28 +0530

    Updated src/services/notifications/index.ts

---
+++ src/services/notifications/index.ts
@@ -0,0 +1,7 @@
+export * from "./constants";
+export { createChannel } from "./createChannel";
+export { createLocalNotificationForAndroid } from "./createLocalNotificationForAndroid";
+export { createLocalNotificationForIOS } from "./createLocalNotificationForIOS";
+export { createLocalNotificationForRemote } from "./createLocalNotificationForRemote";
+export { createNotificationObject } from "./createNotificationObject";
+export { registerPushNotifications } from "./registerPushNotifications";
