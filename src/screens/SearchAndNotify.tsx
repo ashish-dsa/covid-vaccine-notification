8ecc8dd7ae61a2006ae3803e08a58d8f5fcfbcb2
Ashish D'sa <ashish.d@hypersonix.io>
2021-05-06 20:18:27 +0530

    Updated src/screens/SearchAndNotify.tsx

---
+++ src/screens/SearchAndNotify.tsx
@@ -0,0 +1,76 @@
+import { NavigationProp } from "@react-navigation/core";
+import { DistrictSearch } from "components/DistrictSearch";
+import { PincodeSearch } from "components/PincodeSearch";
+import { PinDistrictToggle } from "components/PinDistrictToggle";
+import { SearchFilters } from "components/SearchFilters";
+import { SubmitSearch } from "components/SubmitSearch";
+import React, { useState } from "react";
+import { StyleSheet } from "react-native";
+import { Appbar, withTheme } from "react-native-paper";
+import { SEARCH_OPTIONS } from "./constants";
+
+const SearchAndNotify = ({ navigation, theme }: { navigation: NavigationProp<any>; theme: ReactNativePaper.Theme }) => {
+  const [allChecked, setAllChecked] = React.useState(false);
+  const [adultsChecked, setAdultsChecked] = React.useState(false);
+  const [notifyChecked, setNotifyChecked] = React.useState(true);
+  const [freeChecked, setFreeChecked] = React.useState(false);
+  const [paidChecked, setPaidChecked] = React.useState(false);
+  const [searchOption, setSearchOption] = useState(SEARCH_OPTIONS.district);
+  const [pincodes, setPincodes] = useState(null);
+  const [district, setDistrict] = useState(null);
+  return (
+    <>
+      <Appbar.Header>
+        <Appbar.BackAction onPress={() => navigation.goBack()} />
+        <Appbar.Content title="Search and Notify" subtitle="" />
+      </Appbar.Header>
+      <PinDistrictToggle searchOption={searchOption} setSearchOption={setSearchOption} />
+      {searchOption === SEARCH_OPTIONS.pincode ? (
+        <PincodeSearch setPincodes={setPincodes} />
+      ) : (
+        <DistrictSearch setDistrict={setDistrict} />
+      )}
+      <SearchFilters
+        allChecked={allChecked}
+        setAllChecked={setAllChecked}
+        adultsChecked={adultsChecked}
+        setAdultsChecked={setAdultsChecked}
+        notifyChecked={notifyChecked}
+        setNotifyChecked={setNotifyChecked}
+        freeChecked={freeChecked}
+        setFreeChecked={setFreeChecked}
+        paidChecked={paidChecked}
+        setPaidChecked={setPaidChecked}
+      />
+      <SubmitSearch
+        allChecked={allChecked}
+        adultsChecked={adultsChecked}
+        notifyChecked={notifyChecked}
+        searchType={searchOption}
+        paidChecked={paidChecked}
+        freeChecked={freeChecked}
+        searchParams={searchOption === SEARCH_OPTIONS.pincode ? pincodes : district}
+        navigation={navigation}
+      />
+    </>
+  );
+};
+const styles = StyleSheet.create({
+  base: {
+    height: "10%",
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  rightButton: {
+    marginVertical: "5%",
+    paddingRight: "12%",
+  },
+  leftButton: {
+    marginVertical: "5%",
+    paddingLeft: "12%",
+  },
+  pincode: {
+    marginHorizontal: "6%",
+  },
+});
+export default withTheme(SearchAndNotify);
