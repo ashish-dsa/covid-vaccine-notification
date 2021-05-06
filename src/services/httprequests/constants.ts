a028fcc1295282d1388859dc7314c540b2d6ed7f
Ashish D'sa <ashish.d@hypersonix.io>
2021-05-06 20:18:27 +0530

    Updated src/services/httprequests/constants.ts

---
+++ src/services/httprequests/constants.ts
@@ -0,0 +1,7 @@
+export const HEADERS = {
+  "user-agent":
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36",
+  origin: "https://www.cowin.gov.in",
+  referer: "https://www.cowin.gov.in/",
+  accept: "application/json, text/plain, */*",
+};
