b1e6c730e950b3c076f2965af727354e5cba90d8
Ashish D'sa <ashish.d@hypersonix.io>
2021-05-06 20:18:28 +0530

    Updated src/services/notifications/models/index.ts

---
+++ src/services/notifications/models/index.ts
@@ -0,0 +1,6 @@
+export type { IAndroidNotification } from "./IAndroidNotification";
+export type { IGenericNotification } from "./IGenericNotification";
+export type { IIOSNotification } from "./IIOSNotification";
+export type { ILocalNotification } from "./ILocalNotification";
+export type { IRemoteNotification } from "./IRemoteNotification";
+
