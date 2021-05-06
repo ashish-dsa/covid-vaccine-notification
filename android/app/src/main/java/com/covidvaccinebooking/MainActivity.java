e25c54eadb7a44b6347bd74f58c1c7d3b7c45055
Ashish D'sa <ashish.d@hypersonix.io>
2021-05-06 20:18:24 +0530

    Updated android/app/src/main/java/com/covidvaccinebooking/MainActivity.java

---
+++ android/app/src/main/java/com/covidvaccinebooking/MainActivity.java
@@ -0,0 +1,62 @@
+package com.covidvaccinebooking;
+
+import com.facebook.react.ReactActivity;
+import android.content.Intent;
+import android.util.Log;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is
+   * used to schedule rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "BookVaccine";
+  }
+
+  public boolean isOnNewIntent = false;
+
+  @Override
+  public void onNewIntent(Intent intent) {
+    super.onNewIntent(intent);
+    isOnNewIntent = true;
+    ForegroundEmitter();
+  }
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+    if (isOnNewIntent == true) {
+    } else {
+      ForegroundEmitter();
+    }
+  }
+
+  public void ForegroundEmitter() {
+    // this method is to send back data from java to javascript so one can easily
+    // know which button from notification or the notification button is clicked
+    String main = getIntent().getStringExtra("mainOnPress");
+    String btn = getIntent().getStringExtra("buttonOnPress");
+    String btn2 = getIntent().getStringExtra("button2OnPress");
+    WritableMap map = Arguments.createMap();
+    if (main != null) {
+      map.putString("main", main);
+    }
+    if (btn != null) {
+      map.putString("button", btn);
+    }
+    if (btn2 != null) {
+      map.putString("button", btn);
+    }
+    try {
+      getReactInstanceManager().getCurrentReactContext()
+          .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit("notificationClickHandle", map);
+    } catch (Exception e) {
+      Log.e("SuperLog", "Caught Exception: " + e.getMessage());
+    }
+  }
+}
