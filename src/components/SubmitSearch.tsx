e4e638999a60468b0a10d524061fd401b6f8d13d
Ashish D'sa <ashish.d@hypersonix.io>
2021-05-08 05:07:01 +0530

    Updated src/components/SubmitSearch.tsx

--- src/components/SubmitSearch.tsx
+++ src/components/SubmitSearch.tsx
@@ -1,62 +1,50 @@
-import { NavigationProp } from "@react-navigation/native";
 import { ROUTES } from "navigation/constants";
 import React, { useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
 import { Button } from "react-native-paper";
+import { ISearchParams } from "screens/models";
+import { addToStorage, STORAGE } from "services/storage";
+import { mockSleep } from "utils/mockSleep";
 
-interface ISubmitSearch {
-  allChecked: boolean;
-  adultsChecked: boolean;
-  notifyChecked: boolean;
-  searchType: string;
-  freeChecked: boolean;
-  paidChecked: boolean;
-  searchParams: any;
-  navigation: NavigationProp<any>;
-}
-export const SubmitSearch = ({
-  allChecked,
-  adultsChecked,
-  notifyChecked,
-  searchType,
-  freeChecked,
-  paidChecked,
-  searchParams,
-  navigation,
-}: ISubmitSearch) => {
+export const SubmitSearch = ({ filters, searchParams, searchType, navigation }: ISearchParams) => {
   const [validateSearchParams, setValidateSearchParams] = useState(true);
+  const filtersString = JSON.stringify(filters);
   const validate = () => {
-    if ((allChecked || adultsChecked) && (paidChecked || freeChecked) && searchParams) {
+    if (
+      (filters.senior || filters.adult) &&
+      (filters.paid || filters.free) &&
+      (filters.covaxin || filters.covishield) &&
+      searchParams
+    ) {
       setValidateSearchParams(false);
     } else {
       setValidateSearchParams(true);
     }
   };
 
-  const executeSearch = async () => {
-    navigation.navigate(ROUTES.SearchResults, {
-      allChecked: allChecked,
-      adultsChecked: adultsChecked,
-      notifyChecked: notifyChecked,
-      freeChecked: freeChecked,
-      paidChecked: paidChecked,
-      searchParams: searchParams,
+  const navigateToResults = async () => {
+    const currentSearchParams: ISearchParams = {
       searchType: searchType,
-    });
-    validate();
+      searchParams: searchParams,
+      filters: filters,
+    };
+    await addToStorage(STORAGE.searchParams, JSON.stringify(currentSearchParams));
+    await mockSleep(1000);
+    //@ts-ignore
+    navigation.navigate(ROUTES.SearchResults, {});
   };
 
   useEffect(() => {
     validate();
     return () => {};
-  }, [allChecked, paidChecked, notifyChecked, freeChecked, adultsChecked, searchParams, searchType]);
+  }, [filtersString, searchParams, searchType]);
   return (
     <>
       <Button
         icon="cloud-search"
         mode="contained"
         disabled={validateSearchParams}
-        onPress={() => executeSearch()}
+        onPress={() => navigateToResults()}
         style={styles.searchButton}
       >
         Search
@@ -68,8 +56,6 @@ export const SubmitSearch = ({
 const styles = StyleSheet.create({
   searchButton: {
     marginHorizontal: "6%",
-  },
-  indicator: {
-    marginVertical: "10%",
+    marginVertical: "4%",
   },
 });
