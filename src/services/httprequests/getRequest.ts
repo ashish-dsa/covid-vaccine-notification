c7899777bc71a8ef3707981cfbb1e4a4f05c10e0
Ashish D'sa <ashish.d@hypersonix.io>
2021-05-08 05:07:01 +0530

    Updated src/services/httprequests/getRequest.ts

--- src/services/httprequests/getRequest.ts
+++ src/services/httprequests/getRequest.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import logErrorToMyService from "services/reporting";
 import { HEADERS } from "./constants";
 
 export const getRequest = async (url: string, params: object = {}) => {
@@ -6,8 +7,10 @@ export const getRequest = async (url: string, params: object = {}) => {
     const data = await axios.get(url, {
       headers: HEADERS,
     });
+    logErrorToMyService(new Error("Successfuly fetched centers for " + url));
     return data;
   } catch (error) {
+    logErrorToMyService(new Error("Failed to fetch centers for " + url));
     return null;
   }
 };
