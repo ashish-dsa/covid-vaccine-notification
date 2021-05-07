9ec68ee4396d7bf976b958333604603f985a1ccf
Ashish D'sa <ashish.d@hypersonix.io>
2021-05-08 05:07:02 +0530

    Updated src/components/Filters/index.ts

---
+++ src/components/Filters/index.ts
@@ -0,0 +1,4 @@
+export { AgeFilter } from "./AgeFilter";
+export { NotifyFilter } from "./NotifyFilter";
+export { PriceFilter } from "./PriceFilter";
+export { VaccineBrandFilter } from "./VaccineBrandFilter";
