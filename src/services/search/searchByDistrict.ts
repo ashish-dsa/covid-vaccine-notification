4b8ae22978b01f5706755d2900650b5ecf7804b3
Ashish D'sa <ashish.d@hypersonix.io>
2021-05-08 05:07:01 +0530

    Updated src/services/search/searchByDistrict.ts

--- src/services/search/searchByDistrict.ts
+++ src/services/search/searchByDistrict.ts
@@ -1,18 +1,32 @@
+import { getDDMMYYYY } from "utils/getDDMMYYYY";
 import { getRequest } from "../httprequests/getRequest";
 
-export const searchByDistrict = async (districtId: string) => {
-  let tempdate = new Date();
-  let date = [
-    tempdate.getFullYear(),
-    ("0" + (tempdate.getMonth() + 1)).slice(-2),
-    ("0" + tempdate.getDate()).slice(-2),
-  ].join("-");
-  const currentDate = date.split("-").reverse().join("-");
-  const DISTRICT_SEARCH_URL = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict?district_id=${districtId}&date=${currentDate}`;
-  const response = await getRequest(DISTRICT_SEARCH_URL);
-  if (response) {
-    return response.data.centers;
-  } else {
+export const searchByDistrict = async (districts: Array<string>) => {
+  if (!districts) {
     return null;
   }
+  const currentDate = getDDMMYYYY();
+  const districtList = [];
+  if (districts[0] !== null) {
+    districtList.push(districts[0].toString().trim());
+  }
+  if (districts[1] !== null) {
+    districtList.push(districts[1].toString().trim());
+  }
+  const promises = [];
+  for (let i = 0; i < districtList.length; i++) {
+    if (districtList[i].length > 0) {
+      const DISTRICT_SEARCH_URL = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict?district_id=${districtList[i]}&date=${currentDate}`;
+      promises.push(getRequest(DISTRICT_SEARCH_URL));
+    }
+  }
+  const multipleResponses = await Promise.all(promises);
+  const collatedResponse = [];
+  for (let i = 0; i < multipleResponses.length; i++) {
+    const singleResponse = multipleResponses[i];
+    if (singleResponse) {
+      collatedResponse.push(...singleResponse.data.centers);
+    }
+  }
+  return collatedResponse.length > 0 ? collatedResponse : null;
 };
