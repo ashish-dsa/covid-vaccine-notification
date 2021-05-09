432b965864a71cd6fc7b472c5fdc2de44242ae1a
Ashish D'sa <ashish.d@hypersonix.io>
2021-05-09 20:20:24 +0530

    v0.0.17

--- src/screens/Appointment.tsx
+++ src/screens/Appointment.tsx
@@ -1,7 +1,9 @@
 import { NavigationProp } from "@react-navigation/native";
+import { javascriptToInject } from "constants/javascriptToInject";
 import React, { useRef } from "react";
 import { Appbar, withTheme } from "react-native-paper";
 import WebView from "react-native-webview";
+import logErrorToMyService from "services/reporting";
 
 interface IAppointment {
   navigation: NavigationProp<any>;
@@ -10,6 +12,9 @@ interface IAppointment {
 const Appointment = ({ navigation, theme }: IAppointment) => {
   const webViewRef = useRef(null);
 
+  const onNavigationStateChange = (navState: any) => {
+    webViewRef.current.injectJavaScript(javascriptToInject);
+  };
   return (
     <>
       <Appbar.Header>
@@ -20,6 +25,12 @@ const Appointment = ({ navigation, theme }: IAppointment) => {
         source={{ uri: "https://selfregistration.cowin.gov.in/" }}
         incognito={true}
         javaScriptEnabled={true}
+        onNavigationStateChange={onNavigationStateChange}
+        onError={syntheticEvent => {
+          const { nativeEvent } = syntheticEvent;
+          logErrorToMyService("WebView error: " + JSON.stringify(nativeEvent));
+        }}
+        startInLoadingState={true}
         //@ts-ignore
         ref={ref => (webViewRef.current = ref)}
       />
